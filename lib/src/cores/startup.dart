82b0274c3f74dde40240b79a29514ee977ad53ca
S E N I N  S E L A S A <53807938+Rosemary18@users.noreply.github.com>
2024-05-15 03:22:49 +0700

    [#5-ZK]: Merging from updated clear Windows OS

--- lib/src/cores/startup.dart
+++ lib/src/cores/startup.dart
@@ -11,13 +11,10 @@ Future<void> startUp(bool isMainWindow) async {
   if (isMainWindow) {
     await windowManager.ensureInitialized();
     WindowOptions windowOptions = const WindowOptions(
-      size: Size(700, 700),
-      minimumSize: Size(700, 700),
+      size: Size(700, 800),
+      minimumSize: Size(700, 800),
       center: true,
-      // backgroundColor: Colors.transparent,
-      // skipTaskbar: false,
       titleBarStyle: TitleBarStyle.normal,
-      // windowButtonVisibility: false,
     );
     windowManager.waitUntilReadyToShow(windowOptions, () async {
       await windowManager.show();
